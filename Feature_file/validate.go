f4a1c7ea94905b79a06cb9f55fb8e7aa62548c3e
uditgupta1010 <66454948+uditgupta1010@users.noreply.github.com>
2021-10-09 22:53:22 +0530

    Update validate.go

--- Feature_file/validate.go
+++ Feature_file/validate.go
@@ -9,34 +9,34 @@ import (
 
 func ValidateUser(user *Data_file.InUser) error {
 	if len(user.Id) == 0 {
-		return fmt.Errorf("user id is required")
+		return fmt.Errorf("REQUIRE USER ID")
 	}
 	if len(user.Name) == 0 {
-		return fmt.Errorf("user name is required")
+		return fmt.Errorf("REQUIRE USER NAME")
 	}
 	if len(user.Email) == 0 {
-		return fmt.Errorf("user email is required")
+		return fmt.Errorf("REQUIRE USER EMAIL")
 	} else {
 		emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 		if !emailRegex.MatchString(user.Email) {
-			return fmt.Errorf("email is not valid")
+			return fmt.Errorf("EMAIL ID IS NOT VALID")
 		}
 	}
 	if len(user.Password) == 0 {
-		return fmt.Errorf("password is required")
+		return fmt.Errorf("REQUIRE PASSWORD")
 	}
 	return nil
 }
 
 func ValidatePost(post *Data_file.InPost) error {
 	if len(post.UserId) == 0 {
-		return fmt.Errorf("userId is required")
+		return fmt.Errorf("REQUIRE USER ID")
 	}
 	if len(post.Caption) == 0 {
-		return fmt.Errorf("caption is required")
+		return fmt.Errorf("REQUIRE CAPTION")
 	}
 	if len(post.ImgUrl) == 0 {
-		return fmt.Errorf("imgUrl is required")
+		return fmt.Errorf("REQUIRE URL OF IMAGE")
 	}
 	return nil
 }
